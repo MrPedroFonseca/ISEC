ce7dbd6bfae61e036b4f074533da0e0f309b3563
MrPedroFonseca <83040665+MrPedroFonseca@users.noreply.github.com>
2021-04-26 15:33:48 +0100

    Add files via upload

---
+++ AM2/Trabalho1/ODE45.m
@@ -0,0 +1,19 @@
+%NEULER  Método de Euler para ED/PVI.
+%   y = NEuler(f,a,b,n,y0) Método numérico para a resolução de um PVI
+%   y'= f(t,y) com t=[a, b] e y(a)=y0 condição inicial  
+%
+%INPUT:
+%   f - função do 2.º membro da Equação Diferencial
+%   [a, b] - extremos do intervalo da variável independente t
+%   n - número de subintervalos ou iterações do método
+%   y0 - condição inicial t=a -> y=y0
+%OUTPUT: 
+%   y - vector das soluções aproximações
+%   y(i+1) = y(i)+h*f(t(i),y(i)) , i =0,1,...,n-1
+%
+%   03/03/2020 - ArménioCorreia .: armenioc@isec.pt 
+
+function [t,y]=ODE45(f,a,b,n,y0,~)
+    h=(b-a)/n;
+    tspan=a:h:b;
+[t,y] = ode45(f,tspan,y0);
